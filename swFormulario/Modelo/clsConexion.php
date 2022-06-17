366a1e204b0211f3d794d9c2ffa6838bcd682cfd
sebastian <62351126+ssbastian@users.noreply.github.com>
2022-06-17 18:10:54 -0500

    .

--- swFormulario/Modelo/clsConexion.php
+++ swFormulario/Modelo/clsConexion.php
@@ -1,9 +1,15 @@
 <?php
-/**
- * clase que se encarga de la conexion con la bd
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
- 
 
+/**
+ * Description of clsConexion
+ *
+ * @author Laura
+ */
 class clsConexion {
 
     private $conexion;
--- swFormulario/Modelo/clsFormularioAcceso.php
+++ swFormulario/Modelo/clsFormularioAcceso.php
@@ -1,8 +1,8 @@
 <?php
+require('modelo/clsFormularioAcceso.php');
 /**
  * @author @juan
  */
- 
 class clsFormularioAcceso {
 
     private $conexion;
@@ -23,6 +23,43 @@ class clsFormularioAcceso {
             $i++;
         }
         return $listadoProductos;
+    }
+
+    public function registrar() {
+        session_start();
+
+       // if (isset($_POST['register'])) {
+
+            $username = $_POST['nombre'];
+            $email = $_POST['correo'];
+            $password = $_POST['sexo'];
+            //$password_hash = password_hash($password, PASSWORD_BCRYPT);
+              $sql = 'INSERT INTO (nombre, correo sexo) VALUES :nombre,:correo,:sexo';
+              $consulta = $this->conexion->getConexion()->query($sql);
+            /*
+              $query = $connection->prepare("SELECT * FROM users WHERE EMAIL=:email");
+              $query->bindParam("email", $email, PDO::PARAM_STR);
+              $query->execute();
+
+              if ($query->rowCount() > 0) {
+              echo '<p class="error">The email address is already registered!</p>';
+              } */
+
+            /*if ($query->rowCount() == 0) {
+                $query = $connection->prepare("INSERT INTO users(USERNAME,PASSWORD,EMAIL) VALUES (:username,:password_hash,:email)");
+                $query->bindParam("username", $username, PDO::PARAM_STR);
+                $query->bindParam("password_hash", $password_hash, PDO::PARAM_STR);
+                $query->bindParam("email", $email, PDO::PARAM_STR);
+                $result = $query->execute();
+
+                if ($result) {
+                    echo '<p class="success">Your registration was successful!</p>';
+                } else {
+                    echo '<p class="error">Something went wrong!</p>';
+                }
+            }*/
+        //}
+           return 0;
     }
 
 }
